2ec51fceafce46c405687abf672e98bc2b4277ec
liuhaitao <liuhaitao@91160.com>
2021-03-31 20:07:23 +0800

    xiugia

--- index.js
+++ index.js
@@ -4,6 +4,3 @@ import rem from './lib/config/rem.js'
 import vant from './lib/config/vant.js'
 console.log('执行');
 // 引入公共css
-import './style/common.scss'
-import './style/health.scss'
-import './style/member.scss'
