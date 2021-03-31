f9e76b2106b08ce0a6b32ec91d2f1eda6feedb3b
liuhaitao <liuhaitao@91160.com>
2021-03-31 19:57:55 +0800

    tijiaos

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 // 引入文件 导出
 import rem from './lib/config/rem.js'
 import vant from './lib/config/vant.js'
-
-console.log(rem);
-export default {
-  rem,
-  vant
-}
+console.log('执行');
+// 引入公共css
+import './style/common.scss'
+import './style/health.scss'
+import './style/member.scss'
