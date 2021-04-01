6df1bd8ba21098288c3a98424f6c63648bcb580f
liuhaitao <liuhaitao@91160.com>
2021-04-01 10:34:12 +0800

    更新错误

--- index.js
+++ index.js
@@ -12,17 +12,13 @@ import './lib/directives/index.js'
 // 引入全局过滤器
 import './lib/filters/index.js'
 
-// 引入mixins全局
-import mixins from './lib/mixins/index.js'
 
 // 引入plugins文件
 import './lib/plugins/fastclick/'
 import './lib/plugins/axios.js'
 import './lib/plugins/prototype.js'
-import './lib/plugins/components.js'
 import router from './lib/plugins/vueRouter'
 
 export {
-  mixins,
   router
 }
